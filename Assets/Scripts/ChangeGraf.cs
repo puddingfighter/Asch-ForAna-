66b91d195d775f998dbb41c918a2aefcd9b6c4fc
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-08-18 16:51:37 +0100

    Add avatars and change cardboards

---
+++ Assets/Scripts/ChangeGraf.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGraf : MonoBehaviour
+{
+    [SerializeField] Material material;
+    [SerializeField] Texture [] boards;
+    
+
+    public void ChangeSlide (int round)
+    {
+        material.mainTexture = boards[round];
+    }
+}
---
+++ Assets/Scripts/GameM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameM : MonoBehaviour
+{
+    [SerializeField] ChangeGraf projector;
+    int round = 0;
+
+    float timer = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0.0f)
+            timer -= Time.deltaTime;
+        else
+        {
+            projector.ChangeSlide(round);
+            round++;
+            timer = 5;
+        }
+    }
+}
